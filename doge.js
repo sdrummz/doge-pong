e15ba2728d5f2dbee8ae004df86825d18f3a04c7
szglabs <sdrummond95@gmail.com>
2021-12-26 09:45:11 -0500

    script files

---
+++ doge.js
@@ -0,0 +1,5 @@
+export default class Doge {
+  constructor(dogeElem) {
+    this.dogeElem = dogeElem;
+  }
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+import Doge from './doge.js';
+
+const doge = new Doge(document.getElementById(doge));
+
+function update(time) {}
